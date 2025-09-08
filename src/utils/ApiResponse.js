0c8133f4a8385541b6aa3b772e9fe70ebe58f5cd
Farhan Pathan <farhanpathan072@gmail.com>
2025-09-08 16:06:11 +0530

    class error solved

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,6 +1,10 @@
-constructor(statusCode, data, message = "Success") {
-    this.statusCode = statusCode;
-    this.data = data;
-    this.message = message;
-    this.success = true;
+class ApiResponse{
+
+    constructor(statusCode, data, message = "Success") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = true;
+    }
+    
 }
